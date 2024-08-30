5a6c07b13781c885ec4b8a06d61e99913410169b
Musa <mbhc4235@gmail.com>
2024-08-30 15:54:09 +0300

    main project

---
+++ keyboard.h
@@ -0,0 +1,16 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include <QObject>
+#include <QWidget>
+#include <QGridLayout>
+#include <QLabel>
+
+class keyboard
+{
+    Q_OBJECT
+public:
+    keyboard();
+};
+
+#endif // KEYBOARD_H
--- wordlemain.h
+++ wordlemain.h
@@ -2,6 +2,10 @@
 #define WORDLEMAIN_H
 
 #include <QMainWindow>
+#include <QAbstractButton>
+#include <QLabel>
+#include <QJsonObject>
+#include <QFile>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class WordleMain; }
@@ -15,7 +19,39 @@ public:
     WordleMain(QWidget *parent = nullptr);
     ~WordleMain();
 
+public slots:
+    void keyClicked(QAbstractButton *key);
+
+
+
 private:
     Ui::WordleMain *ui;
+    bool isGameStopped = false;
+    QString wordle="";
+    QStringList quizWords;
+    QStringList dictionary;
+    QJsonObject statistics;
+
+
+private slots:
+    void newWordle();
+    void gameStarted();
+    void gameEnded();
+    void winEvent();
+    void loseEvent();
+    void setStatistics();
+
+    void on_key_ENTER_clicked();
+    void on_key_REMOVE_clicked();
+    void on_wordleBtn_clicked();
+    void on_helpBtn_clicked();
+    void on_statsBtn_clicked();
+
+    int emptyLabelIndex(QList<QLabel *> &labelList);
+    void fillDictionary(QStringList &dictionary, QFile &file);
+    void fillJsonObject(QJsonObject &jsonObject, QFile &file);
+    QList<QLabel *>& availableLabels(QList<QLabel *> &labelList);
+    void updateJsonFile(QFile &file, QJsonObject &jsonObject);
+
 };
 #endif // WORDLEMAIN_H
